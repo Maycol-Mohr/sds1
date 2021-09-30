f94c7c44cbdcf769b8b2b3f0e85569f9a5a681fb
Maycol Michel Mohr <maycolmohr@yahoo.com.br>
2021-09-30 16:08:17 +0200

    Configurando front-web para producao

--- front-web/src/pages/Charts/index.tsx
+++ front-web/src/pages/Charts/index.tsx
@@ -22,7 +22,7 @@ import { buildBarSeries, getPlatformChartData, getGenderChartData } from './help
      series: []
  }
 
- const BASE_URL = 'http://localhost:8080'
+ const BASE_URL = 'https://sds1-maycol.herokuapp.com';
 
 const Charts = () => {
     const [barChartData, setBarChartData] = useState<BarChartData[]>([]);
